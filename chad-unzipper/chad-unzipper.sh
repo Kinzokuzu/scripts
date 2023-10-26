7009631635e63674bd293f11a59bc6a0893a9433
Kinzokuzu <sauljcervantes2@gmail.com>
2023-10-26 10:30:39 -0700

    began target file functionality

--- chad-unzipper/chad-unzipper.sh
+++ chad-unzipper/chad-unzipper.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# filename
 file_name=$1 
+target_file_type=$2
+
 # holds output of the file command
 file_command_output=$(file -b $file_name) 
 # delimits file_command_output by space
-IFS=" " read -ra STRING <<< "$file_command_output"
+IFS=" " read -ra FILE_COMMAND_STRING<<< "$file_command_output"
 # file type is in STRING[0]
-file_type="${STRING[0]}"
+file_type="${FILE_COMMAND_STRING[0]}"
 
-echo "file type is $file_type"
+# TODO: combine the following if and case statements into a loop that will continue
+#       to exicute until we reach our target file type or until we tried too hard :(
+
+# check if we're at the target file type
+if [[ $(echo $(file -b $file_name | grep -q ASCII; echo $?)) == "0" ]]; then
+    echo "$file_name is already a $target_file_type file"
+fi
+
 case $file_type in
     # add decompression commands here
     Zip)
